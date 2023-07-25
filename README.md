6d417358332d8f556221f038a9e92d5126e769cf
Andrew Pikul <ajpikul@gmail.com>
2023-07-25 15:28:11 +0000

    Add logging levels and colors

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # ilog
 
+Notes: Some changes were made. We now have Warn and Debug level interfaces that can be turned on and off.
+In simple logger, it's a Level(DEBUG|INFO|WARN|ERROR|NONE) function, in zap, it's a structure member Level
+I realized in zap we can't use most of the sugar logger interface with this structure, and I'm leaning towards just using
+zap entirely. Also, tests don't implement any of this yet.
+
 `ilog` provides an interface (LoggerInterface) that specifies three functions:
 * Init() error
 * Info(string)
@@ -8,8 +13,25 @@
 You must choose (or write) a backend:
 
 * EmptyLogger- Logging is off in this case
+```
+defaultLogger = &ilog.EmptyLogger{}
+defaultLogger.Init()
+```
 * SimpleLogger- Unbuffered writes to stderr or another file descriptor
+```
+defaultLogger = &ilog.SimpleLogger{}
+defaultLogger.Init()
+```
 * ZapWrap- A backend of uber-go/zap
+```
+defaultLogger = &ilog.ZapWrap{}
+type ilog.ZapWrap struct { 
+ Sugar bool // If true support printf and key-value pairs
+ Paths []string // Out, default is stderr
+ ZapLogger *zap.Logger // Access after init
+ SugarLogger *zap.SugaredLogger // Access after init
+}
+```
 * TestLogger- A backend that calls t.Log or b.Log (has seperate constructor which accepts as an argument the test being run)
 
 Use the `ilog_test.go` file as an example of how to set up loggers. Or TODO: write readme for each one
--- ilog.go
+++ ilog.go
@@ -7,11 +7,28 @@ import (
 	"testing"
 )
 
+const (
+	DEBUG int = iota
+	INFO
+	WARN
+	ERROR
+	NONE
+)
+
+const (
+	RESET  = "\033[0m"
+	RED    = "\033[31m"
+	YELLOW = "\033[33m"
+	GREEN  = "\033[32m"
+)
+
 // LogerInterface defines a simple interface to be used for logging. NOTE: Originally logr was used, but more features lead to less efficiency.
 type LoggerInterface interface {
 	Init() error
 	Info(string)
 	Error(string)
+	Warn(string)
+	Debug(string)
 }
 
 // EmptyLogger is a logger that can be used to turn off logging entirely.
@@ -30,13 +47,27 @@ func (l *EmptyLogger) Info(output string) {
 func (l *EmptyLogger) Error(output string) {
 }
 
+// Warn is EmptyLogger's blank Error method
+func (l *EmptyLogger) Warn(output string) {
+}
+
+// Debug is EmptyLogger's blank Error method
+func (l *EmptyLogger) Debug(output string) {
+}
+
 // This statement ensures we're fufilling the interface we intend to
 var _ LoggerInterface = &EmptyLogger{}
 
 // SimpleLogger is a simple logger that writes to stderr or a path it's given. It is NOT safe for concurrent use.
 type SimpleLogger struct {
-	Path string
-	file *os.File
+	Path   string
+	file   *os.File
+	level  int
+	prefix bool
+}
+
+func (l *SimpleLogger) Level(level int) {
+	l.level = level
 }
 
 // Init attaches SimpleLogger to some path
@@ -44,19 +75,53 @@ func (l *SimpleLogger) Init() error {
 	var err error
 	if len(l.Path) == 0 {
 		l.file = os.Stderr
+		l.prefix = true
 	} else {
 		l.file, err = os.OpenFile(l.Path, os.O_APPEND|os.O_WRONLY, 0644)
+		l.prefix = false
 	}
 	return err
 }
 
 // Info writes the info string to the output for SimpleLogger
 func (l *SimpleLogger) Info(output string) {
+	if l.level > INFO {
+		return
+	}
 	_, _ = l.file.WriteString(output + "\n")
 }
 
 // Error writes the error string to the output for SimpleLogger
 func (l *SimpleLogger) Error(output string) {
+	if l.level > ERROR {
+		return
+	}
+	if l.prefix {
+		_, _ = l.file.WriteString(RED + output + RESET + "\n")
+		return
+	}
+	_, _ = l.file.WriteString(output + "\n")
+}
+
+func (l *SimpleLogger) Warn(output string) {
+	if l.level > WARN {
+		return
+	}
+	if l.prefix {
+		_, _ = l.file.WriteString(YELLOW + output + RESET + "\n")
+		return
+	}
+	_, _ = l.file.WriteString(output + "\n")
+}
+
+func (l *SimpleLogger) Debug(output string) {
+	if l.level > DEBUG {
+		return
+	}
+	if l.prefix {
+		_, _ = l.file.WriteString(GREEN + output + RESET + "\n")
+		return
+	}
 	_, _ = l.file.WriteString(output + "\n")
 }
 
@@ -64,10 +129,12 @@ var _ LoggerInterface = &SimpleLogger{}
 
 // ZapWrap produces a uber-zap logging connection
 type ZapWrap struct {
-	// Sugar is a flag to indicate whether we shoudl use a Sugared logger
+	// Sugar is a flag to indicate whether we should use a Sugared logger
 	Sugar bool
 	// Paths lets us set the logging paths, otherwise we use stderr
 	Paths []string
+	// Level is a debugging level
+	Level int
 	// ZapLogger is the underlying ZapLogger
 	ZapLogger *zap.Logger
 	// SugarLogger is the underlying SugaredLogger
@@ -76,15 +143,29 @@ type ZapWrap struct {
 	infoFunc func(output string)
 	// errorFunc is the function called by the Error() method
 	errorFunc func(output string)
+	debugFunc func(output string)
+	warnFunc  func(output string)
 }
 
-// Init starts a production level zap logger, which we use since we don't use all the same logging levels as Zap. It will switch the info or error func depending on whether or not its a sugared logger.
+// Init starts a production level zap logger, which we use since we don't use all the same logging levels as Zap. It will switch the info or error func depending on whether or not its a sugared logger
 func (z *ZapWrap) Init() error {
 	config := zap.NewProductionConfig()
 	if len(z.Paths) > 0 {
 		config.OutputPaths = z.Paths
+		switch z.Level {
+		case DEBUG:
+			config.Level.SetLevel(zap.DebugLevel)
+		case INFO:
+			config.Level.SetLevel(zap.InfoLevel)
+		case WARN:
+			config.Level.SetLevel(zap.WarnLevel)
+		case ERROR:
+			config.Level.SetLevel(zap.ErrorLevel)
+		case NONE:
+			config.Level.SetLevel(zap.DPanicLevel)
+		}
 	}
-	z.ZapLogger, _ = config.Build(zap.AddCallerSkip(2))
+	z.ZapLogger, _ = config.Build(zap.AddCallerSkip(2)) // Can add more callers here with zap.AddCaller
 	if z.Sugar {
 		z.SugarLogger = z.ZapLogger.Sugar()
 		z.infoFunc = func(output string) {
@@ -93,12 +174,24 @@ func (z *ZapWrap) Init() error {
 		z.errorFunc = func(output string) {
 			z.SugarLogger.Error(output)
 		}
+		z.debugFunc = func(output string) {
+			z.SugarLogger.Debug(output)
+		}
+		z.warnFunc = func(output string) {
+			z.SugarLogger.Warn(output)
+		}
 	} else {
 		z.infoFunc = func(output string) {
 			z.ZapLogger.Info(output)
 		}
 		z.errorFunc = func(output string) {
 			z.ZapLogger.Error(output)
+		}
+		z.debugFunc = func(output string) {
+			z.Debug(output)
+		}
+		z.warnFunc = func(output string) {
+			z.Warn(output)
 		}
 	}
 	return nil
@@ -112,6 +205,14 @@ func (z *ZapWrap) Info(output string) {
 // Error is ZapWraps Error method, just a wrapper for z.errorFunc
 func (z *ZapWrap) Error(output string) {
 	z.errorFunc(output)
+}
+
+func (z *ZapWrap) Warn(output string) {
+	z.warnFunc(output)
+}
+
+func (z *ZapWrap) Debug(output string) {
+	z.debugFunc(output)
 }
 
 var _ LoggerInterface = &ZapWrap{}
@@ -133,9 +234,17 @@ func (t *testLogger) Info(output string) {
 	t.t.Log(output)
 }
 
-// Error supplies the most sever testing method of testLogger
+// Error supplies the most severe testing method of testLogger
 func (t *testLogger) Error(output string) {
 	t.t.Error(output)
+}
+
+func (t *testLogger) Warn(output string) {
+	t.t.Error(output)
+}
+
+func (t *testLogger) Debug(output string) {
+	t.t.Log(output)
 }
 
 // Init is the required Init function of a LoggerInterface for testLogger
